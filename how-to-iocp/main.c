b806e3107d06780c7212ce2ed9e6e9c03a819a93
CGQAQ <m.jason.liu@outlook.com>
2023-05-11 17:42:56 +0800

    backup

--- how-to-iocp/main.c
+++ how-to-iocp/main.c
@@ -1,3 +1,21 @@
+#include <Windows.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {}
+#include "utils.h"
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        eprintf("%s [files to listen]\n", argv[0]);
+        return RET_CODE_ERROR;
+    }
+
+    HANDLE* handles = (HANDLE)malloc(sizeof(HANDLE) * (argc - 1));
+    for (int i = 1; i < argc; ++i) {
+        // GetFileAttributes()
+        // CreateIoCompletionPort();
+    }
+
+    free(handles);
+
+    return 0;
+}
