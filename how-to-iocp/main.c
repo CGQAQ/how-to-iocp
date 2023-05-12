98796914fb20e608d07093fb15664ce9da351454
CGQAQ <m.jason.liu@outlook.com>
2023-05-12 10:32:27 +0800

    printLastError

--- how-to-iocp/main.c
+++ how-to-iocp/main.c
@@ -3,19 +3,58 @@
 
 #include "utils.h"
 
+#define WCHAR_BUF_SIZ (1024)
+
 int main(int argc, char* argv[]) {
+    LPCWCHAR wchar_buf = malloc(WCHAR_BUF_SIZ * sizeof(WCHAR));
+
     if (argc < 2) {
         eprintf("%s [files to listen]\n", argv[0]);
         return RET_CODE_ERROR;
     }
 
-    HANDLE* handles = (HANDLE)malloc(sizeof(HANDLE) * (argc - 1));
+    HANDLE* hFiles = (HANDLE)malloc(sizeof(HANDLE) * (argc - 1));
+    memset(hFiles, 0, 0);
     for (int i = 1; i < argc; ++i) {
-        // GetFileAttributes()
+        const char* path = argv[i];
+
+        int buffer_size = MultiByteToWideChar(
+            CP_ACP, MB_PRECOMPOSED, path, -1 /* null-terminated string */,
+            wchar_buf, WCHAR_BUF_SIZ /* get size */);
+
+        if (buffer_size == 0) {
+            DWORD err = GetLastError();
+            switch (err) {
+                case ERROR_INSUFFICIENT_BUFFER:
+                    break;
+                case ERROR_INVALID_FLAGS:
+                    break;
+                case ERROR_INVALID_PARAMETER:
+                    break;
+                case ERROR_NO_UNICODE_TRANSLATION:
+                    break;
+
+                default:
+                    unreachable(
+                        "MultiByteToWideChar erroring an error that is not "
+                        "defined");
+                    break;
+            }
+        }
+
+        DWORD attr = GetFileAttributes(path);
+        if (attr == INVALID_FILE_ATTRIBUTES) {
+            printLastError();
+        }
+
         // CreateIoCompletionPort();
     }
 
-    free(handles);
+    for (HANDLE* h = hFiles; h < hFiles + argc - 1; ++h) {
+        if (*h != NULL) {
+        }
+    }
+    free(hFiles);
 
     return 0;
 }
--- how-to-iocp/utils.c
+++ how-to-iocp/utils.c
@@ -1,9 +1,29 @@
 #include "utils.h"
 
+#include <Windows.h>
 #include <assert.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+void printLastError() {
+    LPSTR lpBuffer = NULL;
+
+    DWORD error_code = GetLastError();
+
+    DWORD msg_len = FormatMessageA(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL,
+        error_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&lpBuffer,
+        0, NULL);
+
+    if (msg_len == 0) {
+        printf_s("Failed to retrieve error message.\n");
+    } else {
+        printf_s("Error Message(%d): %s", error_code, lpBuffer);
+    }
+
+    LocalFree(lpBuffer);
+}
 
 // print to stderr
 void eprintf(const char* fmt, ...) {
